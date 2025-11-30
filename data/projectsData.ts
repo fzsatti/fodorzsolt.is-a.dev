ade11ea2fa9c49d363baaa9b0545fcd310549999
Fodor Zsolt <fodorzsolt92@gmail.com>
2025-11-30 08:48:31 +0200

    temp project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -5,6 +5,13 @@ interface Project {
   imgSrc?: string
 }
 
-const projectsData: Project[] = []
+const projectsData: Project[] = [
+  {
+    title: 'Home Temperature Monitor',
+    description: `A DIY project that monitors the temperatures of the water in my home's heating system, and the exterior temperature, using a NodeMCU microcontroller and DS18B20 temperature sensors.xc`,
+    imgSrc: '/static/images/home-temp/home_temp.png',
+    href: '/blog/home-temperature-monitor',
+  },
+]
 
 export default projectsData
